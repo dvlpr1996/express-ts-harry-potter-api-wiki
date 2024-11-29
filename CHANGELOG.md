c5b2c833ef958d0fadb64133a9d7ae70422f5b35
dvlpr1996 <nimajahanbakhshian@gmail.com>
2024-11-29 16:36:17 +0330

    Update .gitignore

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-All notable changes to dvlpr1996/express-typescript-harry-potter-api-wiki will be documented in this file
-
-## 0.1.0 (2024-11-21)
-
-- initial release
--- server.ts
+++
@@ -1,8 +0,0 @@
-// Server setup and app initialization
-import app from "./src/app";
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
