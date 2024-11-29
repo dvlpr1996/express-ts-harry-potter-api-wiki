db6d0108e5dd231a96d65fe59903aaacb9a92d88
dvlpr1996 <nimajahanbakhshian@gmail.com>
2024-11-29 16:38:33 +0330

    implement app.ts and server.ts file

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+// Express application setup (entry point)
+// import 'module-alias/register';
+import express, { Application } from 'express';
+// import { PrismaClient } from '@prisma/client';
+// import userRoutes from './routes/userRoutes';
+
+// Create an instance of Express
+const app: Application = express();
+
+// const prisma = new PrismaClient();
+
+app.use(express.json());
+
+
+// app.use('/api/users', userRoutes);
+
+export default app;
