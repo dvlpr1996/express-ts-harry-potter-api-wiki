8cc3bb4ece1cf56d93ff4df3d10b79b22b44f671
dvlpr1996 <nimajahanbakhshian@gmail.com>
2024-11-29 18:37:15 +0330

    implement rateLimitConfig

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 // Express application setup (entry point)
 // import 'module-alias/register';
-import express, { Application } from 'express';
-// import { PrismaClient } from '@prisma/client';
-// import userRoutes from './routes/userRoutes';
+import express, { Application, Request, Response, NextFunction } from 'express';
+import notFoundErrorHandling from './middlewares/notFoundErrorHandling';
+import globalErrorHandling from './middlewares/globalErrorHandling';
+import rateLimitConfig from './config/rateLimitConfig';
 
-// Create an instance of Express
+
+// todo :: mw => check the env file
+
 const app: Application = express();
-
-// const prisma = new PrismaClient();
 
 app.use(express.json());
 
+app.use(rateLimitConfig);
 
-// app.use('/api/users', userRoutes);
+
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  throw new Error(`The Route '${req.originalUrl}' Does Not Exists`);
+});
+
+app.use(notFoundErrorHandling);
+app.use(globalErrorHandling);
 
 export default app;
